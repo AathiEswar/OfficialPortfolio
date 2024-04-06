a09491485b7ba87c1efd6eb59bfef5e71d95d3b3
AathiEswar <aathi28082003@gmail.com>
2024-04-06 15:55:37 +0530

    initial

---
+++ script.js
@@ -0,0 +1,36 @@
+// document.addEventListener("DOMContentLoaded", (event) => {
+//     gsap.registerPlugin(ScrollTrigger)
+
+
+//     const mm = gsap.matchMedia();
+//     const t1 = gsap.timeline();
+//     mm.add("(max-width: 800px)", () => {
+
+//         document.querySelector(".menu-btn" , addEventListener("click" , ()=>{
+    
+
+//             t1.from(".menu-a" , {
+//                 x : 200,             
+//                 stagger : 0.2,
+//                 // duration : .5,
+//             })
+//             .to(".menu-a" , {
+//                 x : 0,
+//             })
+    
+//         }))
+      
+
+
+//     }
+//     )
+    
+ 
+
+
+//    });
+
+
+//    function runGsap (){
+
+//    }
