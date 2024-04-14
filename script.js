e734e405ce2b9c66576f797dbc8312b2d29724cc
AathiEswar <aathi28082003@gmail.com>
2024-04-14 16:39:10 +0530

    day 6

--- script.js
+++ script.js
@@ -120,10 +120,34 @@ document.addEventListener("DOMContentLoaded", (event) => {
   });
   gsap.to(".intro-screen", {
     y: -1 * window.innerHeight - 400,
-    delay: 4,
+    delay: 5.5,
     duration: 1.5,
     ease: "power3.inOut",
   });
+  gsap.fromTo(
+    ".loading-page",
+    { opacity: 1 },
+    {
+      opacity: 0,
+      display: "none",
+      duration: 1.5,
+      delay: 3.5,
+    }
+  );
+  
+  gsap.fromTo(
+    ".logo-name",
+    {
+      y: 50,
+      opacity: 0,
+    },
+    {
+      y: 0,
+      opacity: 1,
+      duration: 2,
+      delay: 0.5,
+    }
+  );
 
   const timelineP = gsap.timeline();
   const titles = gsap.utils.toArray(".p-innerText");
