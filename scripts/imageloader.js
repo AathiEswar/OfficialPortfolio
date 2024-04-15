7b0ae653eba790c9f3bd1a17bb09eaab5957bdd7
AathiEswar <aathi28082003@gmail.com>
2024-04-15 19:06:25 +0530

    day 8

---
+++ scripts/imageloader.js
@@ -0,0 +1,15 @@
+
+
+function preload_image(im_url) {
+    let img = new Image();
+    img.src = im_url;
+  }
+  
+  preload_image("../assets/cropped-HB-logo.png");
+  preload_image("../assets/enfuse.png");
+  preload_image("../assets/experience/pexels-yunus-tuğ-7957992.jpg");
+  preload_image("../assets/cropped-indianprincess.png");
+  preload_image("../assets/experience/pexels-roberto-nickson-2661176.jpg");
+  preload_image("../assets/experience/pexels-alfredo-kayesse-18877738.jpg");
+  preload_image("../assets/cropped-hoppymobility\ website.png");
+  preload_image("../assets/experience/mokhalad-musavi-ilEGYgpaB1k-unsplash.jpg");
