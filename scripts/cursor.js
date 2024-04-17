711384900bd18747922b9e0ccf9329d6d8a9d846
AathiEswar <aathi28082003@gmail.com>
2024-04-17 21:55:21 +0530

    day 10

--- scripts/cursor.js
+++ scripts/cursor.js
@@ -6,9 +6,32 @@ document.addEventListener("mousemove" , (e) =>{
    })
 
 
-   const cursorABout = document.querySelector(".cursor-about");
+const cursorABout = document.querySelector(".cursor-about");
 
-   document.addEventListener("mousemove" , (e) =>{
-    cursorABout.style.left = e.pageX + "px";
-    cursorABout.style.top = e.pageY + "px";
-      })
+document.addEventListener("mousemove" , (e) =>{
+cursorABout.style.left = e.pageX + "px";
+cursorABout.style.top = e.pageY + "px";
+  })
+
+
+const cursorHero = document.querySelector(".cursor-hero");
+
+document.addEventListener("mousemove" , (e) =>{
+  cursorHero.style.left = e.pageX + "px";
+  cursorHero.style.top = e.pageY + "px";
+    })
+
+
+
+
+// const github = document.querySelector(`[data-social="github"]`);
+// github.addEventListener("mouseover" , (e)=>{
+//   text.innerHTML = "linkedin"
+// })
+
+const text = document.querySelector(".cursor-text p");
+const textLong = text.innerText.length;
+text.innerHTML = text.innerText.split('').map(
+  (char , i) => 
+  `<span style="transform:rotate(${(i * 360) / textLong}deg)"> ${char} </span>`
+).join('');
